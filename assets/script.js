4d005305538da58a149e0455fad8ed00db678398
dd <140358105+ddurandev@users.noreply.github.com>
2023-10-20 19:36:46 -0400

    pseudo

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+// Timer function, when timer hits 0 end quiz
+// Interval, conditional for when timer hits 0
+
+// Quiz function
+//Display quiz and the questions
+
+// Next question function and check answer
+// Move to next question, if selected answer is wrong, subtract time
+// When all questions have been answered end quiz
+// Conditional plus display next question
+
+// Save score
+// Local storage
