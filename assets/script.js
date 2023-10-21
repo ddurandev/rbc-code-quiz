6a58cd64a611b450358e1801f388582bd5685905
David Duran <dd@Davids-MBP.fios-router.home>
2023-10-21 16:21:52 -0400

    added questions

--- assets/script.js
+++ assets/script.js
@@ -27,7 +27,39 @@
 var startBtn = document.getElementById('startBtn');
 var questionBox = document.getElementById('questionBox');
 var timerEl = document.getElementById('timer');
+var questionEl = document.getElementById('question');
 var remainTime = 60;
+var choicesSec = document.getElementById('choices');
+
+var questions = [
+    {
+        question: 'What does HTML stand for?',
+        choices: ['Hyper Text Makeup Language', 'Hippo Text Markup Language', 'Hyper Text Markup Language', 'Hyper Test Markup Language'],
+        answer: 'Hyper Text Markup Language'
+    },
+    {
+        questions: 'What does CSS stand for?',
+        choices: ['Cascading Stem Sheets', 'Casting Style Sheets', 'Cascading Style Sheeps', 'Cascading Style Sheets'],
+        answer: 'Cascading Style Sheets',
+    },
+    {
+        questions: 'Which tag do you link your CSS file?',
+        choices: ['header tag', 'footer tag', 'link tag', 'body tag'],
+        answer: 'link tag',
+    },
+    {
+        questions: 'Which tag do you link your JavaScript file?',
+        choices: ['title tag', 'script tag', 'body tag', 'head tag'],
+        answer: 'script tag',
+    },
+    {
+        questions: 'In what secion do you keep the link tag?',
+        choices: ['head', 'body', 'footer', 'header'],
+        answer: 'head',
+    }
+];
+
+var questionIndex = 0;
 
 // Timer function
 function minTimer() {
@@ -47,8 +79,14 @@ function minTimer() {
 };
 
 function displayQuestions() {
-
-}
+    questionEl.textContent = questions[questionIndex].question;
+    choicesSec.innerHTML = '';
+    for (let index = 0; index < questions[questionIndex].choices.length; index++) {
+        var button = document.createElement('button');
+        button.textContent = questions[questionIndex].choices[index];
+        choicesSec.append(button);
+    }
+};
 
 function quizDone() {
 
@@ -56,4 +94,14 @@ function quizDone() {
 
 startBtn.addEventListener('click', function () {
     minTimer();
+    displayQuestions();
 });
+
+choicesSec.addEventListener('click', function(event) {
+    //check answer function
+    if(event.target.textContent != questions[questionIndex].answer) {
+        remainTime -= 5;
+    }
+    questionIndex++;
+    displayQuestions();
+})
